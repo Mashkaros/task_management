a83a20d4999c5cddd90cdf2df8874caa7f202a4a
Ulia Chikileva <mashkaro77@gmail.com>
2020-01-27 13:23:06 +0300

    signIn method added, bcrypt for password added

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,9 +1,10 @@
 import { Repository, EntityRepository } from "typeorm";
 import { User } from "./user.entity";
+import * as bcrypt from 'bcrypt';
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { messageResponse } from "src/tasks/dto/message-response.dto";
 import { ConflictException, InternalServerErrorException } from "@nestjs/common";
-import * as bcrypt from 'bcrypt';
+
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -11,7 +12,9 @@ export class UserRepository extends Repository<User> {
         const { username, password } = authCredentialsDto;
         const user = new User();
         user.username = username;
-        user.password = password;
+        user.salt = await bcrypt.genSalt();
+        user.password = await this.hashPassword(password, user.salt);
+        console.log(user.password);
         try {
             await user.save();
         } catch (error) {
@@ -26,5 +29,17 @@ export class UserRepository extends Repository<User> {
             statusCode: 201,
             message: `User ${username} was successfully created`,
         }
+    };
+    async validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+        const { username, password } = authCredentialsDto;
+        const user = await this.findOne({ username });
+        if (user && await user.validatePassword(password)) {
+            return user.username;
+        }
+        return null;
+    };
+
+    private async hashPassword(password: string, salt: string): Promise<string> {
+        return bcrypt.hash(password, salt);
     }
 }
